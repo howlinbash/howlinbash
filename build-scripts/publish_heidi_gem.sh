e8c181718d6f3001d2013e467228785633254ad3
Howlin Bash <howlinbash@posteo.de>
2017-07-31 15:32:25 +0100

    Fix _config switching and commmiting between heidi and howlinbash

--- build-scripts/publish_heidi_gem.sh
+++ build-scripts/publish_heidi_gem.sh
@@ -5,6 +5,9 @@
 make_gem $live_theme
 sed -i "/^version/{s!$old_version!$new_version!}" $conf
 gem push $new_gem
-git add $gemspec $conf
+git add $gemspec
+git update-index --no-assume-unchanged $conf
+git add -p $conf
 git commit -m "Bump version to $new_version"
 git tag -a v$new_version -m "my version $new_version"
+git update-index --assume-unchanged $conf
