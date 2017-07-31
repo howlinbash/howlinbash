9d734c56792d7e0ce3d238b1a5ab05e657f9fafb
Howlin Bash <howlinbash@posteo.de>
2017-07-31 11:05:54 +0100

    Add theme version bump when publishing gem

--- build-scripts/publish_heidi_gem.sh
+++ build-scripts/publish_heidi_gem.sh
@@ -3,7 +3,8 @@
 # bump gem version; push gem; commit version bump.
 
 make_gem $live_theme
+sed -i "/^version/{s!$old_version!$new_version!}" $conf
 gem push $new_gem
-git add $gemspec
+git add $gemspec $conf
 git commit -m "Bump version to $new_version"
 git tag -a v$new_version -m "my version $new_version"
